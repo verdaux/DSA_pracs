123ad9d01711cb2d89d6cf23a57bd6b34e944e84
PB <52748502+verdaux@users.noreply.github.com>
2022-12-22 18:26:08 +0530

    Initial commit.

---
+++ src/datastructures/Main.java
@@ -0,0 +1,17 @@
+package datastructures;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        printItems(10);
+    }
+    
+    public static void printItems(int n)
+    {
+        for (int i=0;i<n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
