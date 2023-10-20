b47a5e0eb37fc748a8ccf5fe70257a888a02ccc3
PB <xachineon@gmail.com>
2023-10-20 09:40:05 +0530

    Leetcode problem find kth node without length.

--- src/LeetCodeProblems/LinkedList/LinkedList.java
+++ src/LeetCodeProblems/LinkedList/LinkedList.java
@@ -143,4 +143,24 @@ public class LinkedList
         }
         return false;
     }
+
+    public Node findKthFromEnd(int k)
+    {
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 0; i < k; i++)
+        {
+            if(fast==null)
+            {return null;}
+            fast = fast.next;
+        }
+
+        while (fast != null)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 }
--- src/LeetCodeProblems/LinkedList/Main.java
+++ src/LeetCodeProblems/LinkedList/Main.java
@@ -4,15 +4,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        LinkedList ll = new LinkedList(0);
+        LinkedList ll = new LinkedList(1);
 
-        ll.append(1);
+        //ll.append(1);
         ll.append(2);
         ll.append(3);
-        ll.append(4);ll.append(5);
+        ll.append(4);
+        ll.append(5);
         ll.printAll();
-
-        System.out.println("The middle node is:: "+ll.findMiddleNode().value);
+        System.out.println("The kth node is:: "+ll.findKthFromEnd(2).value);
+        //System.out.println("The middle node is:: "+ll.findMiddleNode().value);
 
         /*System.out.println("The removed last element is :: "+ll.removeLast().value);
         ll.printAll();
