aa7959c914ee476f26dda1d6acb5681ce79e428a
PB <xachineon@gmail.com>
2023-11-02 11:33:55 +0530

    Leetcode practice problem to find if LL has loop.

--- src/LeetCodeProblems/LinkedListPractice/LinkedList.java
+++ src/LeetCodeProblems/LinkedListPractice/LinkedList.java
@@ -95,4 +95,31 @@ public class LinkedList
             return null;
         }
     }
+
+    /*
+    Write a method called hasLoop that is part of the linked list class.
+    The method should be able to detect if there is a cycle or loop present in the linked list.
+    The method should utilize Floyd's cycle-finding algorithm, also known as the "tortoise and hare" algorithm, to determine the presence of a loop efficiently.
+     */
+    public boolean hasLoop()
+    {
+        Node fast = head;
+        Node slow = head;
+        while
+        (
+                fast!=null
+                &&
+                        fast.next!=null
+        )
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
