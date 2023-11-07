fa6768343d61003af248d88578200139b399acb2
PB <xachineon@gmail.com>
2023-11-07 11:46:00 +0530

    Leetcode practice problem partition list.

--- src/LeetCodeProblems/LinkedListPractice/Main.java
+++ src/LeetCodeProblems/LinkedListPractice/Main.java
@@ -13,5 +13,18 @@ public class Main
         ll.printAll();
         //System.out.println("The middle node has value:: "+ll.findMiddleNode().value);
         System.out.println("The kth element from end for value is :: "+ll.findKthFromEnd(1).value);
+
+        LinkedList llPartition = new LinkedList(3);
+        llPartition.append(8);
+        llPartition.append(5);
+        llPartition.append(10);
+        llPartition.append(2);
+        llPartition.append(1);
+
+        llPartition.printAll();
+
+        llPartition.partitionList(5);
+
+        llPartition.printAll();
     }
 }
