2e2f47abec07eb332417254210084c6daa1d8cee
PB <xachineon@gmail.com>
2024-08-05 11:00:35 +0530

    Created doubly linked list and append method also.

---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,67 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList
+{
+    Node head = null;
+    Node tail = null;
+    int length = 0;
+
+    public DoublyLinkedList(int value)
+    {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length =1;
+    }
+
+    public Node getHead()
+    {
+        return head;
+    }
+
+    public Node getTail()
+    {
+        return tail;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public void printList()
+    {
+        System.out.println("The linked list has following elements:: \n");
+        Node curr = head;
+        while (curr!=null)
+        {
+            System.out.println(curr.value);
+            curr = curr.next;
+        }
+    }
+
+    public void printAll()
+    {
+        System.out.println("Head node:: "+getHead().value);
+        System.out.println("Tail node:: "+getTail().value);
+        System.out.println("Length of linked list is:: "+getLength());
+        printList();
+    }
+
+    public void append(int value)
+    {
+        Node lastNode = new Node(value);
+        Node curr = head;
+        if (length>0)
+        {
+            tail.next = lastNode;
+            lastNode.prev = tail;
+        }
+        else
+        {
+            head = lastNode;
+        }
+        tail = lastNode;
+        length++;
+    }
+}
---
+++ src/DoublyLinkedList/Main.java
@@ -0,0 +1,13 @@
+package DoublyLinkedList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        DoublyLinkedList ll = new DoublyLinkedList(1);
+        ll.append(2);
+        ll.append(3);
+        ll.append(4);
+        ll.printAll();
+    }
+}
