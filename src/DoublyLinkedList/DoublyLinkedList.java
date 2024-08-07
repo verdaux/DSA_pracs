ec73f3459bced6ebe53b42620c9ba651b2c8faa7
PB <xachineon@gmail.com>
2024-08-07 09:14:00 +0530

    Remove first and prepend methods added.

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -31,19 +31,22 @@ public class DoublyLinkedList
 
     public void printList()
     {
-        System.out.println("The linked list has following elements:: \n");
-        Node curr = head;
-        while (curr!=null)
+        if(getLength()>0)
         {
-            System.out.println(curr.value);
-            curr = curr.next;
+            System.out.println("The linked list has following elements:: \n");
+            Node curr = head;
+            while (curr != null)
+            {
+                System.out.println(curr.value);
+                curr = curr.next;
+            }
         }
     }
 
     public void printAll()
     {
-        System.out.println("Head node:: "+getHead().value);
-        System.out.println("Tail node:: "+getTail().value);
+        System.out.println(getHead()!=null?"Head node:: "+getHead().value:"head is null");
+        System.out.println(getTail()!=null?"Tail node:: "+getTail().value:"tail is null");
         System.out.println("Length of linked list is:: "+getLength());
         printList();
     }
@@ -83,5 +86,40 @@ public class DoublyLinkedList
             length=0;
         }
         return nodeRemoved;
+    }
+
+    public void prepend(int value)
+    {
+        Node firstNode = new Node(value);
+        if(length>0)
+        {
+            head.prev = firstNode;
+            firstNode.next = head;
+            head = firstNode;
+        }
+        else
+        {
+            head = firstNode;
+            tail = firstNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst()
+    {
+        Node firstRemoveCandidate = head;
+        if (length>1)
+        {
+            head = head.next;
+            head.prev = null;
+            length--;
+        }
+        else if (length==1)
+        {
+            head = null;
+            tail = null;
+            length=0;
+        }
+        return firstRemoveCandidate;
     }
 }
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -5,12 +5,21 @@ public class Main
     public static void main(String[] args)
     {
         DoublyLinkedList ll = new DoublyLinkedList(1);
-        ll.append(2);
+        /*ll.append(2);
         ll.append(3);
         ll.append(4);
+        ll.printAll();*/
+
+        /*System.out.println("The removed node has value:: "+ll.removeLast().value);
+        ll.printAll();*/
+
+        ll.prepend(0);
+        ll.prepend(-1);
         ll.printAll();
 
-        System.out.println("The removed node has value:: "+ll.removeLast().value);
+        System.out.println("\nThe removed first node has value:: "+ll.removeFirst().value+"\n");
+        System.out.println("\nThe removed first node has value:: "+ll.removeFirst().value+"\n");
+        System.out.println("\nThe removed first node has value:: "+ll.removeFirst().value+"\n");
         ll.printAll();
     }
 }
